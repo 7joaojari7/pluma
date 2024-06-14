89b83a86e2c0261d23a5d3459ba450a1a1aeed33
João Jari <166957313+7joaojari7@users.noreply.github.com>
2024-06-13 22:49:09 -0300

    added lunoentry.pas
    dll entry point

---
+++ lunoentry.pas
@@ -0,0 +1,19 @@
+library Luno;
+
+uses 
+    lunoloc, 
+    lunomath, 
+    lunoparse,
+    sysutils, 
+    math, 
+    DateUtils;
+
+exports 
+    locdriver_setup, 
+    entity_setup,
+    actor_setup,
+    locdriver_start_event,
+    locdriver_destroy;
+
+begin 
+end.
