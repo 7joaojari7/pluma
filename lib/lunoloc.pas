1c7e5ed0a7410ab0bff66ed84877c84bd7acd6b5
João Jari <166957313+7joaojari7@users.noreply.github.com>
2024-06-14 00:24:02 -0300

    Add files via upload

--- lib/lunoloc.pas
+++ lib/lunoloc.pas
@@ -11,99 +11,146 @@ const
     LOCDRIVER_DEBUG = true;
 
 type
-    ENTITY_MODE_LIST = (EM_STANDBY, EM_MOVING); 
+    ENTITY_STATE_LIST = (EM_STANDBY, EM_MOVING); 
 
     entity = record
-        mode: ENTITY_MODE_LIST;
+        state: ENTITY_STATE_LIST;
         pos: vec2;
-        last_vpos: ^vec2; // Changed to pointer
-        next_vpos: ^vec2; // Changed to pointer
+        model: vec2_array;
+        size: vec2;
+        last_pos: ^vec2; 
+        next_pos: ^vec2; 
     end;
 
-    ld_error = (
-        ld_vlist_out_of_bounds = 0,
-        ld_aaaa = 1
-    );
-
-    entity_ptr = ^entity;
+    actor = record
+        body: entity; 
+        movspeed: single;
+        delta_s: single;
+        delta_t: single;
+    end;
 
     locdriver = record
         vertex_list: vec2_array;
-        vlist_index: integer;
-        actor: entity_ptr;
+        vindex: integer;
+        player: actor;
     end;
-
-    locdriver_ptr = ^locdriver;
 
     LD_SETUP_FLAGS = (
         LD_FORMAT_VERTEX,
         LD_COME_AS_YOU_ARE
     );
 
+    locdriver_ptr = ^locdriver;
+    actor_ptr = ^actor;
+
     function locdriver_setup(
         vertex_list: vec2_array;
-        actor: entity_ptr;
-        initial_pos: vec2; // Changed to vec2
+        player: actor; 
         flags__: LD_SETUP_FLAGS
     ): locdriver_ptr;
 
-    function locdriver_start_event(Ld: locdriver_ptr): ld_error;
+    function entity_setup(pos: vec2; model: vec2_array): entity; export;
+    function actor_setup(body: entity): actor; export;
+    procedure locdriver_start_event(ld_s: locdriver_ptr); export;
 
-    procedure locdriver_destroy(ld: locdriver_ptr);
+    procedure locdriver_destroy(ld: locdriver_ptr); export;
 
 implementation
 
-function locdriver_setup(vertex_list: vec2_array; actor: entity_ptr; initial_pos: vec2; flags__: LD_SETUP_FLAGS): locdriver_ptr;
+function locdriver_setup(vertex_list: vec2_array; player: actor; flags__: LD_SETUP_FLAGS): locdriver_ptr;
 var
-    j: integer;
+    i, j: integer;
     ld_s: locdriver_ptr;
 begin
-    new(ld_s);
-    new(ld_s^.actor);
-    new(ld_s^.actor^.last_vpos);
-    new(ld_s^.actor^.next_vpos);
+    new(ld_s); 
 
-    ld_s^.actor^ := actor^; // Assign the whole record
+    ld_s^.player := player; // Assign the whole record
 
-    for j := Low(vertex_list) to High(vertex_list) do
+    for i := 0 to (High(vertex_list) - 1) do
     begin
-        if (initial_pos.x = vertex_list[j].x) and (initial_pos.y = vertex_list[j].y) then
+        if((ld_s^.player.body.pos.x = vertex_list[i].x) and (ld_s^.player.body.pos.y = vertex_list[i].y)) then
         begin
-            ld_s^.actor^.pos := vertex_list[j];
-
-            if j > 0 then
-                ld_s^.actor^.last_vpos := @vertex_list[j - 1]
-            else
-                ld_s^.actor^.last_vpos := nil;
-
-            if j < High(vertex_list) then
-                ld_s^.actor^.next_vpos := @vertex_list[j + 1]
-            else
-                ld_s^.actor^.next_vpos := nil;
-
-            if LOCDRIVER_DEBUG then
+            ld_s^.vindex := i;
+            if i = 0 then
             begin
-                writeln('actor current pos: ', ld_s^.actor^.pos.x, ' ', ld_s^.actor^.pos.y);
-                if ld_s^.actor^.last_vpos <> nil then
-                    writeln('actor last pos: ', ld_s^.actor^.last_vpos^.x, ' ', ld_s^.actor^.last_vpos^.y)
-                else
-                    writeln('actor last pos: nil');
-                if ld_s^.actor^.next_vpos <> nil then
-                    writeln('actor next pos: ', ld_s^.actor^.next_vpos^.x, ' ', ld_s^.actor^.next_vpos^.y)
-                else
-                    writeln('actor next pos: nil');
+                ld_s^.player.body.last_pos := @vertex_list[i];
+            end
+            else
+            begin
+                ld_s^.player.body.last_pos := @vertex_list[i-1];
             end;
+            ld_s^.player.body.next_pos := @vertex_list[i+1];
+        
+            if(LOCDRIVER_DEBUG = true) then
+            begin
+                writeln('ld_s^.player.body.pos : '          + FloatToStr(ld_s^.player.body.pos.x)       + ' ' + FloatToStr(ld_s^.player.body.pos.y));
+                writeln('ld_s^.player.body.last_pos : '     + FloatToStr(ld_s^.player.body.last_pos^.x) + ' ' + FloatToStr(ld_s^.player.body.last_pos^.y));
+                writeln('ld_s^.player.body.next_pos  : '    + FloatToStr(ld_s^.player.body.next_pos^.x) + ' ' + FloatToStr(ld_s^.player.body.next_pos^.y));
+            end;
+        
         end;
     end;
 
     locdriver_setup := ld_s;
 end;
 
-function locdriver_start_event(Ld: locdriver_ptr): ld_error;
+procedure locdriver_start_event(ld_s: locdriver_ptr);
+var
+    cateto: vec2;
+    distance: single;
+    angle: single;
 begin
-    // Implement this function if needed
-    // Currently, it doesn't do anything
+    cateto.x := (ld_s^.player.body.next_pos^.x * ld_s^.player.body.next_pos^.x) - (ld_s^.player.body.pos.x * ld_s^.player.body.pos.x);
+    cateto.y := (ld_s^.player.body.next_pos^.y * ld_s^.player.body.next_pos^.y) - (ld_s^.player.body.pos.y * ld_s^.player.body.pos.y);
+    distance := Sqrt(cateto.x + cateto.y);
+    angle := Q_atan(cateto.y / cateto.x);
+
+    if(LOCDRIVER_DEBUG = true) then
+    begin
+        writeln('cateto x : '   + FloatToStr(cateto.x));
+        writeln('cateto y : '   + FloatToStr(cateto.y));
+        writeln('distance : '   + FloatToStr(distance));
+        writeln('tan      : '   + FloatToStr(cateto.y / cateto.x));
+        writeln('angle    : '   + FloatToStr(angle));
+    end;
 end;
+
+
+function entity_setup(pos: vec2; model: vec2_array): entity;
+var
+    en_s: entity; 
+begin
+    new(en_s.last_pos); 
+    new(en_s.next_pos); 
+
+    en_s.pos        := pos;
+    en_s.model      := model;
+    en_s.size.x     := 0;
+    en_s.size.y     := 0;
+    en_s.last_pos   := nil; 
+    en_s.next_pos   := nil;  
+
+    if(LOCDRIVER_DEBUG = true) then
+    begin
+        writeln('en_s.pos : ' + FloatToStr(en_s.pos.x) + ' ' + FloatToStr(en_s.pos.y));
+        writeln('en_s.size : ' + FloatToStr(en_s.size.x) + ' ' + FloatToStr(en_s.size.y));
+    end;
+
+    entity_setup := en_s;
+end;
+
+function actor_setup(body: entity): actor;
+var
+    act_s: actor;
+begin
+    act_s.body          := body;
+    act_s.movspeed      := 0; 
+    act_s.delta_s       := 0; 
+    act_s.delta_t       := 0; 
+
+
+    actor_setup := act_s;
+end; 
 
 procedure locdriver_destroy(ld: locdriver_ptr);
 begin
