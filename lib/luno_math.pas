ee22a456f9f16ca905ce0523d8428715352feca4
João Jari <166957313+7joaojari7@users.noreply.github.com>
2024-06-17 00:35:51 -0300

    Update luno_math.pas

--- lib/luno_math.pas
+++ lib/luno_math.pas
@@ -21,8 +21,9 @@ uses math, sysutils;
 const
     // Nossos valores customizados
     Q_PI = 3.14159;         // não precisamos de uma aproximação muito larga de PI, apenas o suficiente
-    MAX_MAP_X_SIZE = 128;   // Largura maxima de um mapa
-    MAX_MAP_Y_SIZE = 128;   // Altura maxima de um mapa
+    MAX_MAP_X_SIZE = 8;   // Largura maxima de um mapa
+    MAX_MAP_Y_SIZE = 8;   // Altura maxima de um mapa
+    EPSILON = 1e-6;
 
 type
     // tipo de vetor bidimensional
@@ -37,7 +38,10 @@ type
     // matriz de vetores bidimensionais
     vec2_array = Array of vec2; 
          
-    function Q_atan(tansrc: Single): Single; export;   
+    function Q_atan(tansrc: Single): single;                                        export;    
+    function Q_sqrrt(x: single): single;                                            export; 
+    function vec2_array_format(list: vec2_array; initial_pos: vec2): vec2_array;    export;
+    function to_degree(ang: single): single;                                        export; 
 
 implementation 
 
@@ -49,5 +53,73 @@ begin
                                                                                         //               Embedded Applications: A Comparative Analysis" 
 end; 
   
+// função para formatar uma matriz de vetores bidimensionais
+function vec2_array_format(     list: vec2_array;                   // lista para receber dados
+                                initial_pos: vec2): vec2_array;     // posição inicial/de referencia                                                     
+var
+    f_list: Array[0..(MAX_MAP_X_SIZE*MAX_MAP_Y_SIZE)] of vec2;      // criando uma matriz para ser a saida
+    i, j:   integer;                                                // indices
+    diff: vec2;                                                     // diferença entre dois pontos
+begin
+    // primeira verificação, feita apenas para descobrir a diferença
+    for i := 0 to High(list) do
+    begin
+        // compare os dois pontos
+        if ((list[i].x = initial_pos.x) and (list[i].y = initial_pos.y)) then
+        begin
+            diff.x := abs(list[i].x);   // utilizamos um valor absoluto ja que apartir daqui o valor inicial deve ser (0, 0)
+            diff.y := abs(list[i].y);
+        end;
+    end;
+    
+    // segunda verificação / inicio de formatação
+    for j := 0 to High(list) do
+    begin
+        // autodescritivo.
+        if (list[j].x < 0) then
+        begin
+            f_list[j].x := list[j].x + diff.x; 
+        end
+        else if (list[j].x > 0) then
+        begin
+            f_list[j].x := list[j].x - diff.x; 
+        end;
 
+        if (list[j].y < 0) then
+        begin
+            f_list[j].y := list[j].y + diff.y; 
+        end
+        else if (list[j].y > 0) then
+        begin
+            f_list[j].y :=list[j].y - diff.y; 
+        end; 
+    end;
+
+    vec2_array_format := f_list;
+end;
+
+function to_degree(ang: single): single;
+begin
+    to_degree := ang * 180 / Q_PI;
+end;
+
+function Q_sqrrt(x: single): single;
+var
+    xn, xn1: single;
+begin
+    if x = 0 then
+        Q_sqrrt := 0
+    else
+    begin
+        // Initial guess (you can choose any positive value)
+        xn := x / 2.0;
+
+        repeat
+        xn1 := xn;
+        xn := 0.5 * (xn1 + x / xn1); // Newton's method iteration
+        until Abs(xn - xn1) < EPSILON; // Convergence criterion
+
+        Q_sqrrt := xn;
+    end;
+end;
 end.
